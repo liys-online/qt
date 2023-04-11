193a81663deb3665ed779fbf6968bfa3d3a0434a
wanghao <wanghao@sinux.com.cn>
2023-04-11 15:27:54 +0800

    [add] 添加模板
    Signed-off-by: wanghao <wanghao@sinux.com.cn>

--- template/ets/native/QtObjectLoader.ts
+++ template/ets/native/QtObjectLoader.ts
@@ -1,13 +1,19 @@
 import { QtDialog } from '../native/QtDialog'
 import { QtPasteBoard } from '../native/QtPasteBoard'
 import { QtInputManager } from '../native/QtInputManager'
+import { QtAudioManager } from '../native/QtAudioManager'
 import { QtWindowManager } from '../native/QtWindowManager'
+import { QtCameraManager } from '../native/QtCameraManager'
+import { QtBtDeviceDiscoveryAgent } from '../native/QtBtDeviceDiscoveryAgent'
 
 let classes = {
     "qtdialog": QtDialog,
     "qtpasteboard": QtPasteBoard,
     "qtinputmanager": QtInputManager,
+    "qtaudiomanager" : QtAudioManager,
     "qtwindowmanager": QtWindowManager,
+    "qtcameramanager" : QtCameraManager,
+    "qtbtdevicediscoveryagent" : QtBtDeviceDiscoveryAgent,
 }
 
 export class QtObjectLoader {
